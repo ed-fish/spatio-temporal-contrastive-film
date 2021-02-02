82578f31025eec0f47b03f0e8b5e00e6d650af94
ed-fish <edward.fish@surrey.ac.uk>
2021-02-02 12:18:21 +0000

    remove overview comments

--- src/main.py
+++ src/main.py
@@ -1,12 +1,3 @@
-
-"""Data has bad format - Requires reference csv for retrieval
-   # todo Construct csv for all videos - fields must include - name, genre, scene, clip_filepath, image
-   # todo Process all videos - extracting frames - do transforms - remove excess data - store references
-   # todo Gather pretrained model - resnet 3d CNN
-   # todo train unsupervised end to end with transforms
-   # todo TOP SECRET STUFF I CANT TELL YOU ABOUT
-
-"""
 from src.preprocessing import dataprocessing as dp
 import pandas as pd
 from src.preprocessing.customdataloader import TransVidDataset
